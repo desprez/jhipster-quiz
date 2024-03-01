2b0912be52a57a6028c9ee7673b7851df1b225b6
Pascal Desprez <pascal.desprez@fr.ibm.com>
2024-03-01 14:25:07 +0100

    implements sort order for questions option

--- src/main/java/com/github/desprez/service/mapper/QuizzMapper.java
+++ src/main/java/com/github/desprez/service/mapper/QuizzMapper.java
@@ -2,12 +2,18 @@ package com.github.desprez.service.mapper;
 
 import com.github.desprez.domain.Quizz;
 import com.github.desprez.domain.User;
+import com.github.desprez.domain.enumeration.DisplayOrder;
+import com.github.desprez.service.dto.QuestionDTO;
 import com.github.desprez.service.dto.QuizzBasicDTO;
 import com.github.desprez.service.dto.QuizzDTO;
 import com.github.desprez.service.dto.UserDTO;
+import java.util.Collections;
+import java.util.Comparator;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 
 /**
@@ -26,4 +32,22 @@ public interface QuizzMapper extends EntityMapper<QuizzDTO, Quizz> {
     @Mapping(target = "id", source = "id")
     @Mapping(target = "login", source = "login")
     UserDTO toDtoUserLogin(User user);
+
+    @AfterMapping
+    default void map(Quizz entity, @MappingTarget QuizzDTO target) {
+        if (DisplayOrder.FIXED.equals(target.getQuestionOrder())) {
+            Collections.sort(
+                target.getQuestions(),
+                new Comparator<QuestionDTO>() {
+                    @Override
+                    public int compare(QuestionDTO b1, QuestionDTO b2) {
+                        return b1.getIndex().compareTo(b2.getIndex());
+                    }
+                }
+            );
+        }
+        if (DisplayOrder.RANDOM.equals(target.getQuestionOrder())) {
+            Collections.shuffle(target.getQuestions());
+        }
+    }
 }
