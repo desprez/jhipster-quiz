0f0e3c44d02b9bc7b571b19ffca43da311491a6e
Pascal Desprez <pascal.desprez@fr.ibm.com>
2024-03-21 17:26:43 +0100

    add publish feature with confirmation

--- src/main/webapp/app/config/navigation.constants.ts
+++ src/main/webapp/app/config/navigation.constants.ts
@@ -2,4 +2,5 @@ export const ASC = 'asc';
 export const DESC = 'desc';
 export const SORT = 'sort';
 export const ITEM_DELETED_EVENT = 'deleted';
+export const ITEM_PUBLISHED_EVENT = 'published';
 export const DEFAULT_SORT_DATA = 'defaultSort';
